c8c109337298e28f27e115dfc864f6f9a3785646
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-04 10:42:58 +0000

    able to read the data and added basic query to launch component

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,31 @@ import logo from "./logo.svg";
 import rocketLogo from "./rocket.jpg";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import Launches from "./components/launches";
+
+const client = new ApolloClient({
+  uri: "https://webdevbootcamp-infamousgodhand.c9users.io:8081/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img
-          alt="Space X app"
-          src={rocketLogo}
-          style={{ width: 200, height: 200, display: "block", margin: "auto" }}
-        />
-        <h1 style={{ textAlign: "center", color: "red" }}>SpaceX App</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            alt="Space X app"
+            src={rocketLogo}
+            style={{
+              width: 200,
+              height: 200,
+              display: "block",
+              margin: "auto"
+            }}
+          />
+          <h1 style={{ textAlign: "center", color: "red" }}>SpaceX App</h1>
+          <Launches />
+        </div>
+      </ApolloProvider>
     );
   }
 }
