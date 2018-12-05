7055160188ae30f9f79865f8322ca3c09eddf42a
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-05 08:22:15 +0000

    launch detail page done

--- client/src/components/launch.js
+++ client/src/components/launch.js
@@ -29,8 +29,50 @@ const Launch = props => {
       {({ loading, error, data }) => {
         if (loading) return <h4>Loading...</h4>;
         if (error) console.log(error);
-        console.log(data);
-        return <h1>Test</h1>;
+        //console.log(data);
+        const {
+          mission_name,
+          flight_number,
+          launch_year,
+          launch_success,
+          rocket: { rocket_id, rocket_name, rocket_type }
+        } = data.launch;
+        return (
+          <div>
+            <h1 className="display-4 my-3 text-center">
+              <span className="text-light">Mission:</span> {mission_name}
+            </h1>
+            <h4 className="mb-3">Launch Details</h4>
+            <ul className="list-group">
+              <li className="list-group-item">
+                Flight Number: {flight_number}
+              </li>
+              <li className="list-group-item">Launch Year: {launch_year}</li>
+              <li className="list-group-item">
+                Launch Success:{" "}
+                <span
+                  className={classNames({
+                    "text-success": launch_success,
+                    "text-danger": !launch_success
+                  })}
+                >
+                  {launch_success ? "YES" : "NO"}
+                </span>
+              </li>
+            </ul>
+            <h4 className="my-3">Rocket Details</h4>
+            <ul className="list-group">
+              <li className="list-group-item">Rocket ID: {rocket_id}</li>
+              <li className="list-group-item">Rocket Name: {rocket_name}</li>
+              <li className="list-group-item">Rocket type: {rocket_type}</li>
+            </ul>
+            <hr />
+
+            <Link className="btn btn-secondary" to="/">
+              Back
+            </Link>
+          </div>
+        );
       }}
     </Query>
   );
